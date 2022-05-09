092fb3ac585dfeefdd27808179192cd3c519b8d9
pdst-lccs <50370310+pdst-lccs@users.noreply.github.com>
2022-05-09 16:50:48 +0100

    Add files via upload

--- search_analysis.py
+++ search_analysis.py
@@ -2,10 +2,10 @@ import random
 import time
 
 def linear_search(v, L):
-    global comparisons
+    #global comparisons
 
     for index in range(len(L)):
-        comparisons = comparisons + 1
+        #comparisons = comparisons + 1
         if L[index] == v:
             return index
             
@@ -32,20 +32,21 @@ def binary_search(v, L):
             
     return -1
 
-'''
+
 # Driver code 1 ...
 some_list = list(range(1000000))
 target = -1 # worst case because -1 never exists
+
 start_time = time.perf_counter()
-#pos = linear_search(target, some_list)
-pos = binary_search(target, some_list)
+pos = linear_search(target, some_list)
+#pos = binary_search(target, some_list)
 end_time = time.perf_counter()
 
 # difference of start and end times gives the execution time
 print("List Length (%d) \t Execution Time (%f)" %(len(some_list), (end_time-start_time) ))
 
-'''
 
+'''
 # Driver code 2 ...
 print("%s\t\t %s\t\t %s" %("List Size", "Found Index", "#Comparisons"))
 for list_size in [1, 10, 100, 1000, 10000, 100000, 1000000]:
@@ -55,3 +56,4 @@ for list_size in [1, 10, 100, 1000, 10000, 100000, 1000000]:
     target = -1 # worst case because -1 never exists
     pos = linear_search(target, some_list)
     print("%d\t\t %d\t\t %d" %(len(some_list), pos, comparisons))
+'''
